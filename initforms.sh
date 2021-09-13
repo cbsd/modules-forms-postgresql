cde3335720be60cedfe5dbab8f5fb516cc2e82fa
olevole <olevole@olevole.ru>
2021-09-13 23:15:01 +0300

    fix include

--- initforms.sh
+++ initforms.sh
@@ -77,7 +77,7 @@ EOF
 
 ${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "postgresql", "201607", "databases/postgresql13-server", "service postgresql restart" );
+INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "postgresql", "201607", "databases/postgresql13-server", "postgresql" );
 COMMIT;
 EOF
 
